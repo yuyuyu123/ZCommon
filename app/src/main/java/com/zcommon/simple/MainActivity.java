ac45293e47a327e5909724be9edc03a2152c742f
Liuliwei <llw1992@ccclubs.com>
2018-06-20 14:29:19 +0800

    add

---
+++ app/src/main/java/com/zcommon/simple/MainActivity.java
@@ -0,0 +1,51 @@
+package com.zcommon.simple;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.cc.android.zcommon.base.RxBasePermissionActivity;
+import com.cc.android.zcommon.img.CameraAndPictureActivity;
+
+
+public class MainActivity extends RxBasePermissionActivity implements  RxBasePermissionActivity.OnSinglePermissionRequestCallBack{
+
+    private ImageView img;
+
+    @Override
+    public void onPermissionAllowed(String s) {
+
+    }
+
+    @Override
+    public void onPermissionDenied(String s) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = findViewById(R.id.img);
+    }
+
+    public void takePhoto(View v) {
+        startActivityForResult(CameraAndPictureActivity.newIntent(this, CameraAndPictureActivity.TYPE_TAKING_PHOTO_AND_CROP, "hello"), 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data == null) {
+            return;
+        }
+        if(requestCode == 1) {
+            final String path = data.getStringExtra("path");
+            Bitmap bitmap = BitmapFactory.decodeFile(path);
+            img.setImageBitmap(bitmap);
+        }
+    }
+}
--- zcommon/src/main/java/com/cc/android/zcommon/utils/StatusBarUtil.java
+++ zcommon/src/main/java/com/cc/android/zcommon/utils/StatusBarUtil.java
@@ -29,6 +29,8 @@ public class StatusBarUtil {
     private static final int FAKE_TRANSLUCENT_VIEW_ID = R.id.statusbarutil_translucent_view;
     private static final int TAG_KEY_HAVE_SET_OFFSET = -123;
 
+
+
     /**
      * 设置状态栏颜色
      *
